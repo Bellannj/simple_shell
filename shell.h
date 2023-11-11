9da86a342aa0339257ed868418032f5d32cb7e32
efa07 <efatariku07@gmmail.com>
2023-11-11 11:12:00 +0300

    updated

--- shell.h
+++ shell.h
@@ -1,14 +1,20 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <errno.h>
+#include <stdarg.h>
 
-#define BUFFER_SIZE 1024
+extern char **environ;
 
-void print(const char *str);
+
+void print(const char* format, ...);
+
 #endif
