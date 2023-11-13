0b77743e442f20f72e1351f82f5c9ff9dbab6eb3
efa07 <efatariku07@gmmail.com>
2023-11-13 11:16:31 +0300

    print

--- print.c
+++ print.c
@@ -1,11 +1,52 @@
 #include "shell.h"
 
-void print(const char *str)
+void print(const char* format, ...)
 {
-    ssize_t bytes_written = write(STDOUT_FILENO, str, strlen(str));
-    if (bytes_written == -1)
+    const char* p;
+    va_list args;
+    va_start(args, format);
+
+   
+    for (p = format; *p != '\0'; ++p)
     {
-        const char *error_msg = strerror(errno);
-        write(STDERR_FILENO, error_msg, strlen(error_msg));
+        if (*p == '%')
+        {
+            
+            ++p; 
+
+            
+            switch (*p)
+            {
+                case 'd':
+                {
+                    int value = va_arg(args, int);
+                    print("%d", value);
+                    break;
+                }
+                case 'f':
+                {
+                    double value = va_arg(args, double);
+                    print("%f", value);
+                    break;
+                }
+                case 's':
+                {
+                    char* value = va_arg(args, char*);
+                    print("%s", value);
+                    break;
+                }
+                default:
+                    putchar(*p);
+                    break;
+            }
+        }
+        else
+        {
+           
+            putchar(*p);
+        }
     }
+
+
+    va_end(args);
 }
