c7d5ef80f18a40b33e66e749fc7ef484214ba31d
efa07 <efatariku07@gmmail.com>
2023-11-13 19:41:53 +0300

    header

--- shell.h
+++ shell.h
@@ -1,20 +1,27 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define BUFFER_SIZE 1024
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
 extern char **environ;
 
-
+char* custom_getline();
 void print(const char* format, ...);
+void free_arguments(char** arguments);
+char** tokenize_arguments(char* command);
 
 #endif
