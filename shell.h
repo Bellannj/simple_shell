575dee51f81354c39e2c8ba88404c78c3e94d769
Bellannj <annabelbliss123@gmail.com>
2023-11-16 09:25:23 -0800

    shell.h debugging

--- shell.h
+++ shell.h
@@ -19,10 +19,10 @@
 
 extern char **environ;
 
-char* custom_getline();
-void print(const char* format, ...);
-void free_arguments(char** arguments);
+char *custom_getline(void);
+void print(const char *format, ...);
+void free_arguments(char **arguments);
 void search_and_execute_command(char **arguments);
-char** tokenize_arguments(char* command);
+char **tokenize_arguments(char *command);
 
 #endif
